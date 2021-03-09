a5eb0df7d7246f6c9fd63a912cd676cfa9cee025
leokorrr <leokorrr@gmail.com>
2021-03-09 15:50:05 +0100

    async fetch requests

--- frontend/src/_services/data.service.js
+++ frontend/src/_services/data.service.js
@@ -9,24 +9,23 @@ export const getLinksFromAPI = async () => {
 export const getPositionsFromAPI = async () => {
     return await axios.get(`${process.env.REACT_APP_BACKEND_API}positions`)
 }
-export const postLinksToAPI = data => {
-    return axios.post(`${process.env.REACT_APP_BACKEND_API}links`, data)
+export const postLinksToAPI = async data => {
+    return await axios.post(`${process.env.REACT_APP_BACKEND_API}links`, data)
 }
-export const postProjectsToAPI = data => {
-    console.log(data)
-    return axios.post(`${process.env.REACT_APP_BACKEND_API}projects`, data)
+export const postProjectsToAPI = async data => {
+    return await axios.post(`${process.env.REACT_APP_BACKEND_API}projects`, data)
 }
-export const postPositionsToAPI = data => {
-    return axios.post(`${process.env.REACT_APP_BACKEND_API}positions`, data)
+export const postPositionsToAPI = async data => {
+    return await axios.post(`${process.env.REACT_APP_BACKEND_API}positions`, data)
 }
-export const deleteLinkFromAPI = (id) => {
-    return axios.delete(`${process.env.REACT_APP_BACKEND_API}links/${id}`)
+export const deleteLinkFromAPI = async id => {
+    return await axios.delete(`${process.env.REACT_APP_BACKEND_API}links/${id}`)
 }
-export const deleteProjectFromAPI = (id) => {
-    return axios.delete(`${process.env.REACT_APP_BACKEND_API}projects/${id}`)
+export const deleteProjectFromAPI = async id => {
+    return await axios.delete(`${process.env.REACT_APP_BACKEND_API}projects/${id}`)
 }
-export const deletePositionFromAPI = (id) => {
-    return axios.delete(`${process.env.REACT_APP_BACKEND_API}positions/${id}`)
+export const deletePositionFromAPI = async id => {
+    return await axios.delete(`${process.env.REACT_APP_BACKEND_API}positions/${id}`)
 }
 
 
