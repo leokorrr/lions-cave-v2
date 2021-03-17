a71b6b51447bf5180a46c764acc06a6a2798baf5
leokorrr <leokorrr@gmail.com>
2021-03-17 11:27:39 +0100

    env fix this commit

--- server.js
+++ server.js
@@ -51,10 +51,13 @@ app.use((error, req, res, next) => {
     })
 })
 if (process.env.NODE_ENV === 'production') {
+    console.log('in')
     app.use(express.static('frontend/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
+} else {
+    console.log('out')
 }
 
 app.listen(PORT, () => console.log(`Server is running, http://localhost:${PORT}`))
